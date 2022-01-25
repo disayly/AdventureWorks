85df9747a2a77aa8e0ec1526d8962d9ce92e36e5
CTJ <minsun16963@gmail.com>
2022-01-25 19:31:02 +0800

    Add files via upload

--- Solution_Exam5.sql
+++ Solution_Exam5.sql
@@ -11,3 +11,4 @@ WHERE soh.shipDate >= DATE_ADD('2001-07-08', INTERVAL -7 DAY)
 AND  soh.shipDate <= '2001-07-08'
 GROUP BY Year(soh.ShipDate), Month(soh.ShipDate), st.TerritoryID
 ORDER BY Year(soh.ShipDate), Month(soh.ShipDate) ASC;
+
