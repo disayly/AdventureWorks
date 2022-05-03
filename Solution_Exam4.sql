d1e15fd1f460af3efd75cf7bc13fe407ee74224a
CTJ <minsun16963@gmail.com>
2022-05-03 15:13:02 +0800

    Delete Solution_Exam4.sql

--- Solution_Exam4.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-c.CustomerID,  st.TerritoryID, st.Name
-FROM customer c
-LEFT JOIN salesterritory st ON c.TerritoryID =  st.TerritoryID
-WHERE c.CustomerID NOT IN (SELECT CustomerID FROM salesorderheader
-WHERE OrderDate BETWEEN '2002-07-16' AND '2002-08-16')
-GROUP BY st.TerritoryID, c.CustomerID
-ORDER BY st.TerritoryID, c.CustomerID
